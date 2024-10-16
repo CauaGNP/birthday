e75529dea9ed54b8e49db7a24ceb372c2220d6f9
CauaGNP <cauagnp13@gmail.com>
2024-10-15 22:52:36 -0300

    adicionando post

---
+++ script/script.js
@@ -0,0 +1,43 @@
+document
+    .querySelector('#escortCheckBox')
+    .addEventListener('change', changeDiv);
+
+document
+    .querySelector('#buttonSubmit')
+    .addEventListener('click', submitUsers)
+    
+function changeDiv(){
+    const escortDiv = document.querySelector('#escortInputs');
+    escortDiv.style.display = escortDiv.style.display === 'flex' ? 'none' : 'flex';
+};
+
+async function submitUsers(){
+    const nameUsers = document.querySelector('#nameInput').value.trim();
+    const escortNum = Number(document.querySelector('#escortValue').value);
+    if(nameUsers === ''){
+        alert('Preencha o campo corretamente!!');
+        
+    }
+
+    try {
+        const result = await fetch("https://parseapi.back4app.com/classes/NameUsersBirthday",{
+            method: "POST",
+            headers: 
+            {"X-Parse-Application-Id": "HDcEEBoBkXkzbC3C4wElSdvEjfddwLpvEdy8i59r",
+            "X-Parse-REST-API-Key": "ViO2ZqLDjvbKaDuMkADaylzVnqcpEMJkCWvXUgkp",
+            "Content-Type": "application/json"},
+            body: JSON.stringify({
+                name: nameUsers,
+                escort: escortNum
+            })
+        })
+        if(!result.ok){
+            alert(`erro no sistema ${result.status}`);
+            throw new Error(`erro no sistema ${result.status}`);
+        }
+        const data = await result.json()
+        console.log(data)
+    } catch (error) {
+        console.error('Erro durante a requisição:', error);
+    }
+}
